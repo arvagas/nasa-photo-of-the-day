7291e1f251f4a80751c0153d02564c296d660749
Arvin Agas <arvagas@gmail.com>
2019-07-17 17:29:12 -0400

    imported all components, everything rendering correctly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios'
 
+// Import components
+import Title from './components/Title'
+import Image from './components/Image'
+import Description from './components/content/Description'
+
 function App() {
+  // Values related to NASA api
   const nasaApi = 'https://api.nasa.gov/planetary/apod?api_key=cySSy2EuIo1USEmHFXesxMwVk4UhxzDBNvn07ESW'
   const apiYear = '2012'
   const apiMonth = '03'
@@ -10,13 +16,22 @@ function App() {
 
   const [title, setTitle] = useState('Testing')
   const [desc, setDesc] = useState('YADA YADA YADA')
-  const [data, setDate] = useState('2019-07-17')
-  const [img, setImg] = useState([])
+  const [date, setDate] = useState('2019-07-17')
+  const [img, setImg] = useState('https://apod.nasa.gov/apod/image/1203/angrysun_friedman_960.jpg')
 
   useEffect(() => {
     axios.get(`${nasaApi}&date=${apiYear}-${apiMonth}-${apiDay}`)
       .then(response => {
         console.log('NASA Photo API online:', response)
+        const nasaTitle = response.data.title
+        const nasaDesc = response.data.explanation
+        const nasaDate = response.data.date
+        const nasaImg = response.data.url
+
+        setTitle(nasaTitle)
+        setDesc(nasaDesc)
+        setDate(nasaDate)
+        setImg(nasaImg)
       })
       .catch(error => {
         console.log('API retrieval failed:', error)
@@ -25,12 +40,13 @@ function App() {
 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className='container'>
+        <Title title={title}/>
+        <Image img={img} />
+        <Description desc={desc} date={date}/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
