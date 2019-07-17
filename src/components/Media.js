69a3626caa09dd630b235dc050b39b7abb1ce274
Arvin Agas <arvagas@gmail.com>
2019-07-17 18:32:02 -0400

    fixed some warnings in media.js

--- src/components/Media.js
+++ src/components/Media.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Media = ({mediaUrl, mediaType}) => {
     if (!mediaUrl) return <h3>Loading...</h3>
 
-    if (mediaType === 'video') return <iframe className='video' src={mediaUrl}></iframe>
-    else return <img className='img' src={mediaUrl}></img>
+    if (mediaType === 'video') return <iframe className='video' title='NASA Video of the Day' src={mediaUrl} alt='NASA VotD'></iframe>
+    else return <img className='img' src={mediaUrl} alt='NASA PotD'></img>
 }
 
 export default Media
