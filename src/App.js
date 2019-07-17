3e1ed607c27d8cb1d0da9c5e4e74d867973aa867
Arvin Agas <arvagas@gmail.com>
2019-07-17 17:00:49 -0400

    axios successfully running in a useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+  const nasaApi = 'https://api.nasa.gov/planetary/apod?api_key=cySSy2EuIo1USEmHFXesxMwVk4UhxzDBNvn07ESW'
+  const apiYear = '2012'
+  const apiMonth = '03'
+  const apiDay = '14'
+
+  const [title, setTitle] = useState('Testing')
+  const [desc, setDesc] = useState('YADA YADA YADA')
+  const [data, setDate] = useState('2019-07-17')
+  const [img, setImg] = useState([])
+
+  useEffect(() => {
+    axios.get(`${nasaApi}&date=${apiYear}-${apiMonth}-${apiDay}`)
+      .then(response => {
+        console.log('NASA Photo API online:', response)
+      })
+      .catch(error => {
+        console.log('API retrieval failed:', error)
+      })
+  }, [apiYear, apiMonth, apiDay])
+
   return (
     <div className="App">
       <p>
