72b9c77e38aa0f619c20fd78612e7e060c9e8ef2
Arvin Agas <arvagas@gmail.com>
2019-07-18 10:40:38 -0400

    utilized lazy/Suspense to have a "loading" screen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import "./App.css";
 
 // Import components
-import Data from './Data'
+const Data = lazy(() => import('./Data'))
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <p>
-          Read through the instructions in the README.md file to build your NASA
-          app! Have fun 🚀!
-        </p>
-      
-        <Data />
-      </div>
+      <Suspense fallback={
+        <div className='App-header'>
+          <h1>NASA Astronomy Picture of the Day</h1>
+          <img className='App-logo' src='https://png2.kisspng.com/sh/bc177aa92d67923a659e890bc4a6a8cf/L0KzQYm3VcA6N6Zxj5H0aYP2gLBuTf5ie5IygdD8aXfxebK0jP9od15zeeZyb37kfH7ohQZqe5D3kZ9sb33wecX7hfUubpD3RadqZkLncYToUBRiOJc7RqMEMES5QYm9UcUzPWk7TaI6OEK4QoW1kP5o/kisspng-nasa-insignia-logo-national-advisory-committee-for-5af2da3a3da0f6.1904618615258650182524.png'></img>
+        </div>}>
+        <div className='container'>
+          {/* <p>
+            Read through the instructions in the README.md file to build your NASA
+            app! Have fun 🚀!
+          </p> */}
+        
+          <Data />
+        </div>
+      </Suspense>
     </div>
   )
 }
