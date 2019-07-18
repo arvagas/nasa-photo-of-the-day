241d1881247ee4d034d5ddbc5d18e8a8f8826d4d
Arvin Agas <arvagas@gmail.com>
2019-07-17 20:28:16 -0400

    split off api call/data into its own file

--- src/App.js
+++ src/App.js
@@ -1,54 +1,19 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "./App.css";
-import axios from 'axios'
 
 // Import components
-import Title from './components/Title'
-import Media from './components/Media'
-import Description from './components/content/Description'
-import DropdownContainer from './components/Dropdown/DropdownContainer'
+import Data from './Data'
 
 function App() {
-  // Values related to NASA api
-  const nasaApi = 'https://api.nasa.gov/planetary/apod?api_key=cySSy2EuIo1USEmHFXesxMwVk4UhxzDBNvn07ESW'
-  const apiYear = '2012'
-  const apiMonth = '03'
-  const apiDay = '14'
-
-  const [title, setTitle] = useState('Testing')
-  const [desc, setDesc] = useState('YADA YADA YADA')
-  const [date, setDate] = useState('2019-07-17')
-  const [mediaUrl, setMediaUrl] = useState('https://apod.nasa.gov/apod/image/1203/angrysun_friedman_960.jpg')
-  const [mediaType, setMediaType] = useState('photo')
-
-  useEffect(() => {
-    axios.get(`${nasaApi}&date=${apiYear}-${apiMonth}-${apiDay}`)
-      .then(response => {
-        console.log('NASA Photo API online:', response)
-        const nasaTitle = response.data.title
-        const nasaDesc = response.data.explanation
-        const nasaDate = response.data.date
-        const nasaMediaUrl = response.data.url
-        const nasaMediaType = response.data.media_type
-
-        setTitle(nasaTitle)
-        setDesc(nasaDesc)
-        setDate(nasaDate)
-        setMediaUrl(nasaMediaUrl)
-        setMediaType(nasaMediaType)
-      })
-      .catch(error => {
-        console.log('API retrieval failed:', error)
-      })
-  }, [apiYear, apiMonth, apiDay])
-
   return (
     <div className="App">
       <div className='container'>
-        <Title title={title}/>
-        <Media mediaUrl={mediaUrl} mediaType={mediaType}/>
-        <Description desc={desc} date={date}/>
-        <DropdownContainer />
+        <p>
+          Read through the instructions in the README.md file to build your NASA
+          app! Have fun 🚀!
+        </p>
+      
+        <Data />
       </div>
     </div>
   )
