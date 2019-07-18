9cefb8ad5c95db8933515aaf69c2882b0cbd5952
Arvin Agas <arvagas@gmail.com>
2019-07-18 14:15:08 -0400

    imported styled-components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {lazy, Suspense} from "react";
 import "./App.css";
+import styled from "styled-components"
 
 // Import components
 const Data = lazy(() => import('./Data'))
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <Suspense fallback={
         <div className='App-header'>
-          <h1>NASA Astronomy Picture of the Day</h1>
+          <h1>Astronomy Picture of the Day</h1>
+          <h2>Powered by NASA</h2>
           <img className='App-logo' src='https://png2.kisspng.com/sh/bc177aa92d67923a659e890bc4a6a8cf/L0KzQYm3VcA6N6Zxj5H0aYP2gLBuTf5ie5IygdD8aXfxebK0jP9od15zeeZyb37kfH7ohQZqe5D3kZ9sb33wecX7hfUubpD3RadqZkLncYToUBRiOJc7RqMEMES5QYm9UcUzPWk7TaI6OEK4QoW1kP5o/kisspng-nasa-insignia-logo-national-advisory-committee-for-5af2da3a3da0f6.1904618615258650182524.png'></img>
         </div>}>
         <div className='container'>
