afe988d10b46ecdc6a24189e5e3aebaba63641eb
Arvin Agas <arvagas@gmail.com>
2019-07-18 16:39:05 -0400

    removed/replaced NASA logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {lazy, Suspense} from "react";
 import "./App.css";
+import circle from './assets/circle.png'
 
 // Import components
 const Data = lazy(() => import('./Data'))
@@ -11,14 +12,13 @@ function App() {
         <div className='App-header'>
           <h1>Astronomy Picture of the Day</h1>
           <h2>Powered by NASA</h2>
-          <img className='App-logo' src='https://png2.kisspng.com/sh/bc177aa92d67923a659e890bc4a6a8cf/L0KzQYm3VcA6N6Zxj5H0aYP2gLBuTf5ie5IygdD8aXfxebK0jP9od15zeeZyb37kfH7ohQZqe5D3kZ9sb33wecX7hfUubpD3RadqZkLncYToUBRiOJc7RqMEMES5QYm9UcUzPWk7TaI6OEK4QoW1kP5o/kisspng-nasa-insignia-logo-national-advisory-committee-for-5af2da3a3da0f6.1904618615258650182524.png'></img>
+          <img className='App-logo' src={circle}></img>
         </div>}>
         <div className='container'>
           {/* <p>
             Read through the instructions in the README.md file to build your NASA
             app! Have fun 🚀!
           </p> */}
-        
           <Data />
         </div>
       </Suspense>
